51057af970f79ad28265f7e0113a701f397eacd1
Gangabhavani622 <gangabhavani0628@gmail.com>
2022-11-26 13:36:15 +0000

    BannerCardItem cp3-1

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,21 @@
+// Write your code here.
+import './index.css'
+
+const BannerCardItem = props => {
+  const {bannerCards} = props
+  const {id, className, headerText, description} = bannerCards
+  const listStyling = `banner-style ${className}`
+  return (
+    <li className={listStyling} key={id}>
+      <div>
+        <h1 className="heading">{headerText}</h1>
+        <p className="para">{description}</p>
+        <button className="buttonEle" type="button">
+          Show More
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default BannerCardItem
